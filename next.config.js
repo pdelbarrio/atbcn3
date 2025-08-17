a89fa8b44d6be7fe01cd010ddc838bec2e02a654
Pablo del Barrio <pablodbb@gmail.com>
2025-08-17 14:39:11 +0200

    updated next.config.js file

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@
 const withSerwist = require('@serwist/next').default({
   swSrc: 'src/sw.ts',
   swDest: 'public/sw.js',
-  skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
-  // Maintain your existing PWA settings
-  reloadOnOnline: true,
-  cacheOnFrontEndNav: true
 });
 
 const nextConfig = {
